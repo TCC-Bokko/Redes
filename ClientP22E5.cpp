ff8719d13b43d2aca04773ba4e440374d0bbc709
Antonio Cardona Costa <antocard@ucm.es>
2018-05-04 12:52:05 +0200

    Add files via upload

--- ClientP22E5.cpp
+++ ClientP22E5.cpp
@@ -1,4 +1,4 @@
-//[Practica 2.2 Ejercicio 4] Servidor Eco TCP
+//[Practica 2.2 Ejercicio 5] Cliente del Servidor Eco TCP
 
 //Red
 #include <sys/socket.h>
@@ -19,6 +19,13 @@
 #include <string.h>
 
 int main(int argc, char** argv){
+	//Debug (comando llamada ./"nombreApp" 127.0.0.1 3000 t
+	std::cout << "argc: " << argc << std::endl; //4
+	std::cout << "argv[0]: " << argv[0] << std::endl; //./client
+	std::cout << "argv[1]: " << argv[1] << std::endl; //127.0.0.1
+	std::cout << "argv[2]: " << argv[2] << std::endl; //3000
+	std::cout << "argv[3]: " << argv[3] << std::endl; //t
+	
 	/////////////////////////////
 	/// TCP
 	////////////////////////////
@@ -33,7 +40,7 @@ int main(int argc, char** argv){
 	hints.ai_family = AF_UNSPEC;         		      	//ipV4 o ipV6 soportado
 	hints.ai_socktype = SOCK_STREAM; 			//UDP SOCK_DGRAM, TCP SOCK_STREAM 
 	
-	//Creamos variables donde almacenaremos los datos del cliente
+	//Creamos variables donde almacenaremos los datos del servidor
 	struct sockaddr src_addr;		
 	socklen_t addr_len = sizeof(struct sockaddr);
 	char host[NI_MAXHOST]; //IP
@@ -51,41 +58,41 @@ int main(int argc, char** argv){
 	//Socket
 	int sd = socket(res->ai_family, res->ai_socktype, 0);	
 
-	//Hacemos el bind
-	bind(sd,res->ai_addr, res->ai_addrlen);
-
-	//Llamamos al listen (TCP)
-	listen(sd, 15);
-	
 	//Limpiamos el puntero
 	freeaddrinfo(res);
 
-	int sd_src = accept(sd, &src_addr, &addr_len);
+	//Connect del cliente
+	int sd_src = connect(sd, &src_addr, addr_len);
 
 	getnameinfo(&src_addr, addr_len, host, NI_MAXHOST, serv, NI_MAXSERV, NI_NUMERICHOST | NI_NUMERICSERV);
 
-	std::cout << "Conexión: " << host << ":" << serv << std::endl;
+	std::cout << "Conectando a: " << host << ":" << serv << std::endl;
+
+	//Mandamos mensaje inicial
+	send(sd_src, argv[3], 80, 0);		
+	
+	//Creamos un contenedor del mensaje lo suficientemente grande.
+	char buffer[256]; 
 
 	//Gestión del envío y recepción de mensajes
-	while (true) {
-		char buffer[256]; //Creamos un contenedor del mensaje lo suficientemente grande.
+	while (buffer != "Q") {
+		
 		
 		//Recibimos el mensaje y almacenamos su tamaño en "s"
 		ssize_t s = recv(sd_src, buffer, 255, 0);
 		
 		//Llamar al cierre si se desconecta el cliente, eso ocurre cuando el núm bytes es 0 (s == 0)
-		if (s == 0){
+		if (s == 0 || buffer[0] == 'Q'){
 			std::cout << "Fin de la conexión. Cerrando." << std::endl;	//Notificamos cierre
 			close(sd_src);							//Cerramos socket
-			break; 								//rompemos el bucle 								//rompemos el bucle
-		 }		
+			break; 								//rompemos el bucle
+		 }	
 
-  		//Mostrar información obtenida
-  		std::cout << "Mensaje:" << buffer << std::endl; //buffer contiene en fin de línea
+		//Actualizamos el mensaje
+		std::cin >> buffer;
 		
-		//Devolvemos el mensaje (eco)
+		//Mandamos el mensaje
 		send(sd_src, buffer, s, 0);
-		
 		
 
 	}
